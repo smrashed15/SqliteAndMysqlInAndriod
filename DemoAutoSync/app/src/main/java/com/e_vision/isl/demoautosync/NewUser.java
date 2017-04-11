68f1628e0d33d8201f3c019dcb5c04d7fe3d7b69
MD RASHEDUL ALAM <smrashedcse@gmail.com>
2017-04-11 17:20:38 +0600

    AndoridMysqlAndSqlite
    MysqlAndSqliteInAndroid

---
+++ DemoAutoSync/app/src/main/java/com/e_vision/isl/demoautosync/NewUser.java
@@ -0,0 +1,48 @@
+package com.e_vision.isl.demoautosync;
+
+import java.util.HashMap;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NewUser extends Activity {
+
+	EditText userName;
+	DBController controller = new DBController(this);
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_new_user);
+		userName = (EditText) findViewById(R.id.userName);
+	}
+
+
+	public void addNewUser(View view) {
+
+		HashMap<String, String> queryValues = new HashMap<String, String>();
+		queryValues.put("userName", userName.getText().toString());
+		if (userName.getText().toString() != null && userName.getText().toString().trim().length() != 0) {
+			controller.insertUser(queryValues);
+			this.callHomeActivity(view);
+		} else {
+			Toast.makeText(getApplicationContext(), "Please enter User name",Toast.LENGTH_LONG).show();
+		}
+	}
+	
+
+
+	public void callHomeActivity(View view) {
+		Intent objIntent = new Intent(getApplicationContext(),MainActivity.class);
+		startActivity(objIntent);
+	}
+
+
+
+	public void cancelAddUser(View view) {
+		this.callHomeActivity(view);
+	}
+}
